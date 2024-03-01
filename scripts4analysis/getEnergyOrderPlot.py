780c8203cf43753c02e8b64b90ec799640916a7a
otayfuroglu <tayfuroglu@gmail.com>
2024-03-01 09:17:31 +0300

    small changes

--- scripts4analysis/getEnergyOrderPlot.py
+++ scripts4analysis/getEnergyOrderPlot.py
@@ -9,28 +9,37 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-#  atoms_list = read("vasp_opt_polymeric_all.extxyz", index=":")
+keyword = "isolated"
+#  keyword = "polymeric"
+
+#  atoms_list = read(f"vasp_opt_{keyword}_all.extxyz", index=":")
 #  atoms_e_list = [(atoms, atoms.get_potential_energy()) for atoms in atoms_list]
 #
 #  atoms_e_list = sorted(atoms_e_list, key=lambda x: x[1])
 #  for atoms, e in atoms_e_list:
-#      write("sorted_opt_vasp_polymeric_all.extxyz", atoms, append=True)
+#      write(f"sorted_opt_vasp_{keyword}_all.extxyz", atoms, append=True)
 #
 #  quit()
 
 
 all_enegies = []
 
+def getNames(atoms_list):
+    names = []
+    for atoms in atoms_list:
+        names += [atoms.info["label"]]
+    return names
+
+
 def getEplot(atoms_list, shift, color, label):
     global all_enegies
     energies = []
     for atoms in atoms_list:
         energy = atoms.get_potential_energy()
-        energies += [energy]
+        energies += [energy / len(atoms)]
     min_e = min(energies)
     for energy in energies:
-        #  energy -= min_e
-        energy /= len(atoms)
+        energy -= min_e
         #  energy = atoms.get_potential_energy()
         #  if any([f"{energy:.3f}" == f"{en_in:.3f}" for en_in in energies]):
             #  print("Equal Energy")
@@ -46,32 +55,35 @@ def getEplot(atoms_list, shift, color, label):
 
 
 struc_type_legends = []
-#  atoms_list = read("../vasp/vasp_opt_polymeric_all.extxyz", index=":")
-#  atoms_list = read("../vasp/vasp_opt_polymeric_all.extxyz", index=":")
-atoms_list = read("/Users/omert/Desktop/alanates/qe/qe_opt_lowest_10_isolated_24atoms.extxyz", index=":")
+
+#  atoms_list = read(f"../vasp/vasp_opt_{keyword}_all.extxyz", index=":")
+#  atoms_list = read(f"../vasp/vasp_opt_{keyword}_all.extxyz", index=":")
+atoms_list = read(f"vasp_opt_initial_structures_with_label_{keyword}.extxyz", index=":")
 #  label = "n2p2 En. Vasp opt. Isolated"
 #  label = "vasp opt. isolated"
 #  label = "minima hopping"
-label = "isolated"
+label = f"{keyword}_VASP_PBE"
 struc_type_legends += [getEplot(atoms_list, shift=0.0, color="b", label=label)]
+#  atoms_list = read(f"sorted_opt_vasp_{keyword}_all.extxyz", index=slice(0,1))
+#  label = f"Vasp Opt MineE {keyword}"
+#  struc_type_legends += [getEplot(atoms_list, shift=0.6, color="c", label=label)]
 
-#  atoms_list = read("tight_vasp_opt_isolated_rotated_v1.extxyz", index=":")
-atoms_list = read("/Users/omert/Desktop/alanates/qe/qe_opt_lowest_10_polymeric_24atoms.extxyz", index=":")
+#  atoms_list = read(f"/Users/omert/Desktop/alanates/qe/opt/qe_opt_{keyword}.extxyz", index=":")
+atoms_list = read(f"vasp_opt_PBE_initial_structures_{keyword}.extxyz", index=":")
+#  names_qe = getNames(atoms_list)
 #  label = "rotated"
 #  label = "Vasp Opt. Initial Polymeric"
-#  label = "n2p2 opt. polymeric"
+#  label = f"n2p2 opt. {keyword}"
 #  label = "nequip opt. isolated"
-label = "polymric"
+label = f"{keyword}_VASP_SCAN"
 struc_type_legends+= [getEplot(atoms_list, shift=0.3, color="r", label=label)]
 
-#  atoms_list = read("sorted_opt_vasp_polymeric_all.extxyz", index=slice(0,1))
-#  label = "Vasp Opt MineE polymeric"
-#  struc_type_legends += [getEplot(atoms_list, shift=0.6, color="c", label=label)]
 
 #  avg_diff = np.array(energies_v1).mean() - np.array(energies_v2).mean()
 #  print(avg_diff)
 
-plt.ylim([min(all_enegies)-0.5, max(all_enegies)+0.5])
+plt.ylim([min(all_enegies)-0.01, max(all_enegies)+0.01])
+plt.ylabel("Relative Energy / atom (eV)")
 plt.legend(handles=struc_type_legends)
-plt.show()
-#  plt.savefig("energyOrder.png")
+#  plt.show()
+plt.savefig(f"vasp_scan_pbe_energyOrder_{keyword}.png")
